d8e7a981dd8b6db55ca5fa12f8657ce46f491099
David Gurovich <david.gurovich@outlook.com>
2023-06-13 09:54:06 -0700

    Explore heading animated. Modal padding fixed.

--- Coding-challenge/src/App.vue
+++ Coding-challenge/src/App.vue
@@ -3,20 +3,18 @@ import Facebook from './components/icons/Facebook.vue';
 import Instagram from './components/icons/Instagram.vue';
 import Modal from './components/icons/Modal.vue';
 import Plus from './components/icons/Plus.vue';
-
-
 </script>
 
 <template>
   <div id='app' class="">
-    
     <div class="flex flex-col pt-[600px] pl-[290px]">
-      <div class="w-[909px] h-[289px]">
-        <h1 class="text-[#FFFFFF] text-[224px] leading-[289px]  font-Didot ">Explore</h1>
+      <transition name="slide" appear>
+        <div class="w-[909px] h-[289px]">
+          <h1  class="text-[#FFFFFF] text-[224px] leading-[289px] font-medium font-Didot ">Explore</h1>
       </div>
+      </transition>
       <div >
-        <button class="flex flex-row items-center gap-x-5 " >
-          <Plus class="transform active:scale-75 transition-transform"/>
+        <button @click="toggleA = !toggleA" class="flex flex-row items-center gap-x-5 " >
           <Modal/>
         </button>
       </div>
@@ -32,10 +30,7 @@ import Plus from './components/icons/Plus.vue';
 
 <style>
 
-
-  @import url('https://fonts.googleapis.com/css2?family=Lato&family=Quicksand:wght@300;400&display=swap');
-
-
+@import url('https://fonts.googleapis.com/css2?family=Lato&family=Quicksand:wght@300;400&display=swap');
 #app {
     background-image: linear-gradient(to left, transparent 70%, #611818 40%),url('./assets/Coding-Challenge-Image.png');
     background-position: center bottom;
@@ -49,4 +44,42 @@ import Plus from './components/icons/Plus.vue';
 body {
   overflow: hidden;
 } 
+.slide-enter { 
+  opacity: 0;
+}
+
+.slide-enter-active {
+  animation: slide-in 1.5s ease-in forwards;
+  transition: opacity .5s;
+}
+
+.slide-leave {
+  
+}
+
+.slide-leave-active {
+  animation: slide-out 2s ease-out forwards; 
+  opacity: 0;
+  transition: opacity 1s;
+}
+
+
+@keyframes slide-in {
+  from {
+    transform: translateX(-500px);
+  }
+  to {
+    transform: translateX(0);
+  }
+}
+
+@keyframes slide-out {
+  from {
+    transform: translateX(0);
+  }
+  to {
+    transform: translateX(500px);
+  }
+}
+
 </style>
--- Coding-challenge/src/components/icons/Modal.vue
+++ Coding-challenge/src/components/icons/Modal.vue
@@ -2,13 +2,15 @@
 
 <template>
     <div class="container mx-auto">
-      <div class="flex justify-center">
+      <div class="flex justify-center
+      ">
         <button
           @click="isOpen = true"
-          class="text-white font-Lato font-light text-[25px]"
+          class="text-white   flex flex-row items-center gap-x-5"
           type="button"
         >
-          More Details
+        <svg class="transform active:scale-75 transition-transform" xmlns="http://www.w3.org/2000/svg" width="43" height="43" viewBox="0 0 43 43"><g><g><path fill="#fff" d="M29.295 22.139h-6.833v7.17h-1.934v-7.17h-6.833v-1.815h6.833v-7.11h1.934v7.11h6.833zM21.5 0C9.626 0 0 9.626 0 21.5S9.626 43 21.5 43 43 33.374 43 21.5A21.5 21.5 0 0 0 21.5 0z"/></g></g></svg>
+          <p class="font-Lato text-[25px] font-light transform active:scale-75 transition-transform">More Details</p>
         </button>
         <transition name="fade" appear>
   
@@ -24,25 +26,23 @@
         >
           <div class="w-[580px] h-[690px] left-[140px] top-[159px] p-6 bg-[#FFFFFF] shadow-xl">
             <div class="flex items-center justify-between">
-              <h3 class="text-[66px] font-Didot font-[700] leading-[85px] text-[#611818] mt-10 pl-10">Explore</h3>
-              <svg
-                @click="isOpen = false"
-                xmlns="http://www.w3.org/2000/svg"
-                class="w-8 h-8 text-red-900 cursor-pointer"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
-                />
-              </svg>
+              <h3 class="text-[66px] font-Didot font-[700] leading-[85px] text-[#611818] mt-12 pl-14">Explore</h3>
+              <div class="text-[#611818] w-[22px] h-[22px] pb-[140px]" @click="isOpen = false">
+                <svg viewPort="32 32 32 32" version="1.1"
+                xmlns="http://www.w3.org/2000/svg" height="100" w="100" >
+                <line x1="1" y1="18" 
+                    x2="18" y2="1" 
+                    stroke="#611818" 
+                    stroke-width="2"/>
+                <line x1="1" y1="1" 
+                    x2="18" y2="18" 
+                    stroke="#611818" 
+                    stroke-width="2"/>
+                </svg>
+            </div>
             </div>
             <div class="mt-4 px-4">
-              <p class="text-left pl-10 leading-[22px] font-Lato text-[18px] w-[442px] h-[345px] text-[#282828]">
+              <p class="text-left pl-10 pt-2 leading-[22px] font-Lato text-[18px] w-[442px] h-[345px] text-[#282828]">
                 Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium 
                 doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore 
                 veritatis et quasi architecto beatae vitae dicta sunt explicabo.
@@ -53,20 +53,22 @@
                 dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis 
                 nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? 
               </p>
-              <button
+              
+              
+            </div>
+            <button
                 id="button"
                 @click="isOpen = false"
-                class="uppercase w-[235px] h-[55px] font-Lato text-[#FFFFFF] text-[18px] border solid #979797 "
+                class="mt-8 mr-[190px] uppercase w-[235px] h-[55px] font-Lato text-[#FFFFFF] text-[18px] border solid #979797"
               >
                 Read More
               </button>
-              
-            </div>
           </div>
         </div></transition>
       </div>
     </div>
   </template>
+
   <script>
     
 
@@ -77,6 +79,8 @@
       };
     },
   };
+
+  
   </script>
 
   <style>
